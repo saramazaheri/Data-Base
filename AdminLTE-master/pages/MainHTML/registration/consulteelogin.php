565178cd2f838c1358811b2ff4f332b17bfe052c
saramazaheri <61461140+saramazaheri@users.noreply.github.com>
2022-01-17 00:03:54 +0330

    Last Update(I hope so)

---
+++ AdminLTE-master/pages/MainHTML/registration/consulteelogin.php
@@ -0,0 +1,27 @@
+<?php
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    //Database connection
+    $con = new mysqli("localhost", "root", "", "consuling center");
+    if($con->connect_error){
+        die("Connection failed: " .$con->connect_error);
+    } else {
+        $stmt = $con->prepare("select email, password, role from users where email = ? and role = 'consultee'");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $stmt_result = $stmt->get_result();
+        if($stmt_result->num_rows > 0) {
+            $data = $stmt_result->fetch_assoc();
+            if($password === $data['password']) {
+                echo "<h2>Login Successful</h2>";
+                //header("Location: adminhome.php");
+                //to do
+            } else {
+                echo "<h2>Login Failed1</h2>";
+            }
+        } else {
+            echo "<h2>Login Failed2</h2>";
+        }
+    }
+?>
